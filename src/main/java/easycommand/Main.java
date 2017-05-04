df22501d45350009eed2b34d4a86d9dc862fd02e
Benoit Theunissen <benoit.theunissen@gmail.com>
2017-05-04 22:09:52 +0200

    build:create project

---
+++ src/main/java/easycommand/Main.java
@@ -0,0 +1,11 @@
+package easycommand;
+
+import spark.Spark;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Spark.get("/hello", (req, res) -> "Hello World");
+    }
+
+}
